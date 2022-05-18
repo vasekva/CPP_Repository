fdd172662d544dcb78dc0f13b44dd59acb44a307
vasekva <vahonin_vasek@mail.ru>
2022-05-18 18:27:32 +0300

    Added an algorithm for adding odd duplicated rows into a new vector<string>

---
+++ algorithms/odd_duplicated_strings/main.cpp
@@ -0,0 +1,44 @@
+#include <unordered_map>
+#include <iostream> 
+#include <vector>
+
+/**
+ * 0(n)
+ *
+ * Напишите функцию, которая принимает на вход массив строк, и
+ * возвращает такой же массив, но без тех строк, которых в исходном массиве
+ * было чётное количество. Оставшиеся в массиве элементы должны идти в том же
+ * порядке друг относительно друга, что и в первоначальном массиве.
+ * */
+
+std::vector<std::string> countFreq(const std::vector<std::string> &arr)
+{
+    std::vector<std::string> newVector;
+    std::unordered_map<std::string, int> map;
+ 
+    for (int i = 0; i < arr.size(); i++)
+		map[arr[i]]++;
+ 
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (map[arr[i]] != -1 && map[arr[i]] % 2 != 0)
+        {
+           newVector.push_back(arr[i]);
+			map[arr[i]] = -1;
+        }
+    }
+    return (newVector);
+}
+ 
+int main()
+{
+    std::vector<std::string> arr = { "one", "two", "three", "three", "two", "three", "what", "1"};
+    
+    std::vector<std::string> newVec = countFreq(arr);
+    
+    for (auto s : newVec)
+        std::cout << s << " ";
+    std::cout << std::endl;
+    
+    return 0;
+}
