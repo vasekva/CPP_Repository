6656d90f7164269a6a742eee7fa9bbb3a2b81b92
vasekva <vahonin_vasek@mail.ru>
2022-06-03 21:35:28 +0300

    Added the ability to make a linkedList with N size and serialize it to a file. Serialize format - 'address_before_saving/node_name/address_to_random_node'

--- linked_list_serialization/main.cpp
+++ linked_list_serialization/main.cpp
@@ -1,51 +1,113 @@
-#include "Data.hpp"
+//#include "Data.hpp"
 
 #include "iostream"
 #include "fstream"
 #include "sstream"
 
-void serialize(Data *ptr, std::ofstream &out_file)
+class ListNode
+{
+	public:
+		ListNode	*m_prev;
+		ListNode	*m_next;
+		ListNode	*m_rand;
+		std::string m_data;
+
+		ListNode(std::string data)
+		{
+			this->m_prev = nullptr;
+			this->m_next = nullptr;
+			this->m_rand = nullptr;
+			this->m_data = data;
+		}
+		~ListNode() {}
+};
+
+void recursion_serialize(ListNode *ptr, std::ofstream &out_file)
 {
 	std::stringstream str_stream;
 	std::string str;
 
-	out_file << ptr->getName();
-	out_file.close();
+	if (ptr == nullptr)
+		return;
+
+	str_stream << ptr << "/"; // param delimeter
+	str_stream << ptr->m_data << "/";
+	if (ptr->m_rand)
+	{
+		str_stream << ptr->m_rand << "/";
+		str_stream >> str;
+	}
+	else
+	{
+		str_stream << "nullptr/";
+	}
+	str_stream >> str;
+	recursion_serialize(ptr->m_next, out_file); // recursion
+
+	str.append("\n"); // data delimeter
+
+	out_file << str;
 }
 
-Data *deserialize(std::ifstream &in_file)
+bool serialize_obj(ListNode *ptr, const std::string &file_name)
 {
-	Data *new_data = nullptr;
-	std::string name;
-
-	in_file >> name;
-	new_data = new Data(name);
-
-	in_file.close();
-	return (new_data);
-}
-
-int main(void)
-{
-	Data		*main_object;
-	Data		*deserialized_obj;
-
-	/**
-	 * Creating an object
-	 */
-	main_object = new Data("Petr");
-	std::cout << "First addr: " << main_object << std::endl;
-
-	/**
-	 * Opening a file for writing to it
-	 */
 	std::ofstream out_file("some_file.txt");
 	if (!out_file)
 	{
 		puts("Couldn't open the file");
-		return (-1);
+		return (false);
 	}
-	serialize(main_object, out_file);
+	recursion_serialize(ptr, out_file); // рекурсивная сериализация списка
+	out_file.close();
+	return (true);
+}
+
+ListNode *deserialize(std::ifstream &in_file)
+{
+	ListNode *new_data = nullptr;
+	std::string name;
+
+	in_file >> name;
+	new_data = new ListNode(name);
+
+	return (new_data);
+}
+
+void create_list(ListNode **tmp_ptr, ListNode **main_obj, size_t N)
+{
+	std::string name = "Node_#";
+
+	for (int i = 0; i < N; i++)
+	{
+		if (i == 0)
+		{
+			*tmp_ptr = new ListNode(name + (std::to_string(i)));
+			*main_obj = *tmp_ptr;
+		}
+		else
+		{
+			(*tmp_ptr)->m_next = new ListNode(name + std::to_string(i));
+			(*tmp_ptr) = (*tmp_ptr)->m_next;
+		}
+	}
+}
+
+int main(void)
+{
+	ListNode		*main_object;
+	ListNode		*deserialized_obj;
+
+	/**
+	 * Creating a linkedList with N size
+	 */
+	ListNode *tmp_ptr;
+	create_list(&tmp_ptr, &main_object, 11);
+
+	/**
+	 * Opening a file for writing to it
+	 */
+	const std::string file_name = "some_file.txt";
+	serialize_obj(main_object, file_name);
 
 	/**
 	 * Opening a file for reading from it
@@ -57,6 +119,7 @@ int main(void)
 		return (-1);
 	}
 	deserialized_obj = deserialize(in_file);
+	in_file.close();
 
 	/**
 	 * Checking and printing deserialized data
@@ -70,8 +133,10 @@ int main(void)
 	std::cout << "Addr after serialization: " << deserialized_obj << std::endl;
 	std::cout << "Its attributes: " << std::endl;
 
-	std::cout << deserialized_obj->getName() << std::endl;
+	std::cout << deserialized_obj->m_data;
 
-	delete main_object;
-	delete deserialized_obj;
+
+// TODO: сделать удаление всего списка
+//  delete main_object;
+//	 delete deserialized_obj;
 }
